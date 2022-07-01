993d44bebe53f6348c14d62f9b28f2c29a09a92f
GG <1908838350@qq.com>
2022-07-01 16:37:53 +0800

    第三次Idea提交测试

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试一下");
         System.out.println("测试一下2");
+        System.out.println("测试一下3");
     }
 }
