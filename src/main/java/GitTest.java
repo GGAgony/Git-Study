4082f7cf4da9d16ba77fc53c945923ad08c56239
GG <1908838350@qq.com>
2022-07-01 16:34:59 +0800

    第一次Idea提交测试

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: {LZG}
+ * @ClassName: GitTest
+ * @Description: TODO
+ * @Date: 2022/7/1 16:34
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("测试一下");
+    }
+}
